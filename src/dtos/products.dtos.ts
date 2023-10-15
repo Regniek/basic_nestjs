69e0731288906d5ebeb6d4521a416beb95c66ddd
Juank Espitia <juanespitia@outlook.com>
2023-10-14 23:22:16 -0500

    feat: finalize course basic nest js

--- src/dtos/products.dtos.ts
+++ src/dtos/products.dtos.ts
@@ -1,20 +1,50 @@
-import exp from "constants";
-
+// El class-validator es una libreria que nos permite validar los datos que se envian en el body de la peticion
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+// con esta dependecia podemos compartir parte d elo que se creo en el class validator como base para otros dtos
+import { PartialType } from '@nestjs/mapped-types';
 // los dtos (data transfer object) son objetos que se utilizan para proteger los datos validar y tipar, evitar hacer referencia a datos que no existan
 
-export class CreateProductDto{
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
-export class UpdateProductDto{
+// el partialType permite extender las validaciones del create y ademas dejarlos opcionales
+export class UpdateProductDto extends PartialType(CreateProductDto) {
   // los signos de pregutna es para que en el update los diferentes parametros queden opcionales
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
+  // @IsString()
+  // readonly name?: string;
+  // @IsString()
+  // readonly description?: string;
+  // @IsNumber()
+  // readonly price?: number;
+  // @IsNumber()
+  // readonly stock?: number;
+  // @IsUrl()
+  // readonly image?: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    // con este atributo el automaticamente quita todos los datos que no se necesitan del payload a partir del dto
+    whitelist: true,
+    // con este no lo ignoras sino que genera una alerta de  los atributos extras
+    forbidNonWhitelisted: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
